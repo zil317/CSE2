54519ddab26325d30cd870ca972058c9b2f5668b
zil317 <zil317@lehigh.edu>
2016-01-29 17:03:02 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+/////////////
+//Zihan Liu
+//lab1
+////Hello World Java Program
+//
+// first compile the program
+//   java HelloWorld.java
+//run the program
+//   java HelloWorld//
+
+// define a class
+public class HelloWorld{
+    
+// add main method
+ public static void main(String[] args) {
+     
+//print the statement with box around it
+     System.out.println("######################");
+     System.out.println("######################");
+     System.out.println("#                    #");
+     System.out.println("# Hello World 2014!! #");
+     System.out.println("#                    #");
+     System.out.println("######################");
+     System.out.println("######################");
+     System.out.println("test");
+     
+     //test/testtest
+ }
+}
